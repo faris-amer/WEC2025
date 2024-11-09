b5fc4f00edd55424c56f8cc9e6cc7ba66d6aa642
Hamza Elkababji <hamza.kababji@gmail.com>
2024-11-08 19:32:21 -0500

    Added routing to the Motors class through the switch case function calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,63 @@
 #include <Arduino.h>
 #include <IRremote.h>
-// put function declarations here:
 
 #define IRPIN 7
 
 int currentcommand;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+Motors rover;
 
+void setup() {
+  Serial.begin(9600);
 
   IrReceiver.begin(IRPIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   if (IrReceiver.decode()) {
     // Serial.println(IrReceiver.decodedIRData.command);
     //IrReceiver.printIRResultShort(&Serial);
+
     if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
         Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
         // We have an unknown protocol here, print more info
         IrReceiver.printIRResultRawFormatted(&Serial, true);
     }
-    switch(IrReceiver.decodedIRData.command){
-      case 69:
-        currentcommand = 1 ;
-      break;
-      case 70:
-        currentcommand = 2 ;
-      break;
-      case 71:
-        currentcommand = 3;
-      break;
-      case 68:
-        currentcommand = 4;
-      break;
-      case 64:
-        currentcommand = 5;
-      break;
-      case 67:
-        currentcommand = 6;
-      break;
-      case 21:
-        currentcommand = 8;
-      break;
-      case 7:
-        currentcommand = 7;
-      break;
-      case 9:
-        currentcommand = 9;
-      break;
-      case 25:
-        currentcommand = 0;
-      break;
-      case 22:
-        currentcommand = 11; // Asterix
-      break;
-      case 13:
-        currentcommand = 12; // Hashtag
-      break;
-      case 24:
-        currentcommand = 13; // Up
-      break;
-      case 82:
-        currentcommand = 14; // Down
-      break;
-      case 8:
-        currentcommand = 15; // Left
-      break;
-      case 90:
-        currentcommand = 16; // Right
-      break;
-      case 28:
-        currentcommand = 17; // Ok
-      break;
+
+    switch (IrReceiver.decodedIRData.command) {
+
+      // Speed Control
+      case 69:  currentcommand = 0;    rover.setSpeed(0);     break;
+      case 70:  currentcommand = 1;    rover.setSpeed(25.5);  break;
+      case 71:  currentcommand = 2;    rover.setSpeed(51);    break;
+      case 68:  currentcommand = 3;    rover.setSpeed(76.5);  break;
+      case 64:  currentcommand = 4;    rover.setSpeed(102);   break;
+      case 67:  currentcommand = 5;    rover.setSpeed(127.5); break;
+      case 7:   currentcommand = 6;    rover.setSpeed(153);   break;
+      case 21:  currentcommand = 7;    rover.setSpeed(178.5); break;
+      case 9:   currentcommand = 8;    rover.setSpeed(204);   break;
+      case 25:  currentcommand = 9;    rover.setSpeed(229.5); break;
+
+      // Boom Arm Control
+      case 22:  currentcommand = 11;   rover.lowerBoom();     break; // Asterisk
+      case 13:  currentcommand = 12;   rover.raiseBoom();     break; // Hashtag
+
+      // Directional Control
+      case 24:  currentcommand = 13;   rover.moveForward();   break; // Up
+      case 82:  currentcommand = 14;   rover.moveBackward();  break; // Down
+      case 8:   currentcommand = 15;   rover.turnLeft();      break; // Left
+      case 90:  currentcommand = 16;   rover.turnRight();     break; // Right
+
+      // Stop Control
+      case 28:  currentcommand = 17;   rover.stop();          rover.stopBoom(); break; // OK
+
+      default:
+        Serial.println("Unknown Command.");
+        rover.stop();
+        break;
     }
+
     Serial.println(currentcommand);
     IrReceiver.resume(); // Enable receiving of the next value
   }
